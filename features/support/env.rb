ff3e1eebf57e2d613dc49f3d1a1c4b184dfd089d
Robert Scott <robert.scott@digital.cabinet-office.gov.uk>
2019-04-29 17:36:52 +0100

    synchronize_with_unload_wait: wait until after loadEventEnd, not domComplete
    the thinkning goes that we should allow the page to run any event handler
    setup code it needs to before we go and start trying to interact with things

--- features/support/env.rb
+++ features/support/env.rb
@@ -136,7 +136,7 @@ module Capybara
     class Base
       def synchronize_with_unload_wait(*args, &block)
         Timeout.timeout(dm_pre_load_wait_time) do
-          until driver.evaluate_script('window.performance.timing.navigationStart < window.performance.timing.domComplete')
+          until driver.evaluate_script('window.performance.timing.navigationStart < window.performance.timing.loadEventEnd')
             # navigationStart has been updated more recently than domComplete, domComplete presumably still
             # carrying the value set when the *old* page got loaded - that means the dom is probably in the
             # process of loading (or at least requesting) a new page. any following page queries will
